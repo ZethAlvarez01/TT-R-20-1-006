33950d737845ec4156cef69744098c156629a0e0
ZethAlvarez01 <zethalvarezh@hotmail.com>
2019-10-08 22:26:56 -0500

    Deteccion de espacios, conexion back front

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template,request
-from reportlab.pdfgen import canvas
+from flask import Flask, render_template,request,make_response
+import pdfkit
 
 app = Flask(__name__)
 
@@ -10,10 +10,16 @@ def index():
 @app.route('/submit',methods=['POST'])
 def get_text():
     text=request.form.get("text")
-    c = canvas.Canvas("tt-r-20-1-006.pdf")
-    c.drawString(50, 50, text)
-    c.save()
-    return c
+    pdf=pdfkit.from_string(text,False)
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'inline; filename=TT-R-20-1-006.pdf'
+    return response
+
+@app.route('/background_process_test')
+def background_process_test():
+    print("Hello")
+    return "nothing"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8555)
+    app.run(debug=True, port=9110)
--- static/script/contador.js
+++ static/script/contador.js
@@ -1,3 +1,9 @@
 function contadorCaracter(obj) {
     document.getElementById("n-caracteres").innerHTML = 'Número de caracteres: ' + obj.value.length;
 }
+
+function detecta(e){
+    if(e.keyCode == 32){
+        alert("Espacio detectado");
+    }
+}
